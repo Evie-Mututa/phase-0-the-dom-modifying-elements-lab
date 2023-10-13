6cd33419883511d6c5e0f4339ed0a06525e8e2a5
Evie-Mututa <evemututa29@gmail.com>
2023-10-13 03:27:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the 'main' element with id 'main'
+document.querySelector('main#main').remove();
+// Create a new 'h1' element with the id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+// Set the text content for the 'h1' element
+newHeader.textContent = "Everlyne Mututa is the champion";
+// Append the 'h1' element to the body
+document.body.appendChild(newHeader);
